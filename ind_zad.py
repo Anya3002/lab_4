ef488389ab13984c619628f99723d3f4d7b6adba
Anya3002 <91079221+Anya3002@users.noreply.github.com>
2021-11-14 17:41:47 +0300

    Add files via upload

---
+++ ind_zad.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Напечатать в обратном порядке последовательность чисел
+# признаком конца которой является 0
+
+
+if __name__ == '__main__':
+    def reverse(string):
+        if len(string) == 0:
+            return string
+        else:
+            return reverse(string[1:]) + string[0]
+    a = str(input("Введите строку: "))
+    print(reverse(a))
+
+# Проверка
+
+    def is_palindrome(string):
+        reversed_string = string[::-1]
+        return string == reversed_string
+
+    print(is_palindrome(''))
+
+
+
+
+
+
+
+
+
+
+
+
+
