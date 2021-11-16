26d376a156f1b2642a9ba66956d46c08f261849a
Anya3002 <91079221+Anya3002@users.noreply.github.com>
2021-11-16 13:30:48 +0300

    Update ind_zad.py

--- ind_zad.py
+++ ind_zad.py
@@ -4,12 +4,13 @@
 # признаком конца которой является 0
 
 
-if __name__ == '__main__':
-    def reverse(string):
+   def reverse(string):
         if len(string) == 0:
             return string
         else:
             return reverse(string[1:]) + string[0]
+        
+    if __name__ == '__main__':
     a = str(input("Введите строку: "))
     print(reverse(a))
 
